32d64b49b9b3a3a4f311903a87fad69c451be71b
milkays <48838938+milkays@users.noreply.github.com>
2021-04-13 00:23:35 -0400

    Created project

---
+++ prac2/practica2/practica2/Controllers/StudentsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace practica2.Controllers
+{
+    [ApiController]
+    [Route("/api/students")]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+        }
+
+        [HttpGet]
+        public List<Student> GetStudents()
+        {
+            List<Student> listStudents = new List<Student>();
+            listStudents.Add(new Student { Name = "Mayra Oropeza" });
+            listStudents.Add(new Student { Name = "Rafaela Gumiel" });
+            listStudents.Add(new Student { Name = "Mauricio Angulo" });
+            listStudents.Add(new Student { Name = "Fabricio Fernandez" });
+            listStudents.Add(new Student { Name = "Camila Medina" });
+            return listStudents;
+        }
+
+        [HttpPost]
+        public Student CreateStudent([FromBody] string studentName)
+        {
+            return new Student()
+            {
+                Name = studentName
+            };
+        }
+
+        [HttpPut]
+        public Student UpdateStudent([FromBody] Student student)
+        {
+            Console.WriteLine("Updated");
+            student.Name = "UPDATED";
+            return student;
+        }
+
+        [HttpDelete]
+        public Student DeleteStudent([FromBody] Student student)
+        {
+            Console.WriteLine("Deleted");
+            return student;
+        }
+    }
+}
---
+++ prac2/practica2/practica2/Student.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace practica2
+{
+    public class Student
+    {
+         public String Name { get; set; }
+
+        
+    }
+}
